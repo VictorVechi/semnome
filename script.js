c85738918e91f756613a1c6315846ba86b0b10ba
Charlie-Pencal <cpencal@gmail.com>
2023-07-08 15:39:22 -0300

    fix: arummado a validação do @ e . no fim do login

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 
-const login = document.getElementById("login").value;
-const senha = document.getElementById("senha")
+const login = document.getElementById("login");
+const senha = document.getElementById("senha");
 const botao = document.getElementById('botao')
 
-botao.addEventListener('click', function (event) {
-    event.preventDefault();
-    if (login.indexOf('@') == -1 || login.indexOf('@') == 0 || login.indexOf('@') == login.length || login.indexOf('.') == -1 || login.indexOf('.') == 0 || login.indexOf('.') == login.length) {
-        alert('Login ou senha inválida')
 
+botao.addEventListener('click', function (botao) {
+    botao.preventDefault();
+    if (login.value.indexOf('@') === -1 || login.value.indexOf('@') === 0 || login.value.indexOf('@') === login.value.length - 1 || login.value.indexOf('.') === -1 || login.value.indexOf('.') === 0 || login.value.indexOf('.') === login.value.length - 1 || login.value.length < 7) {
+        alert('Login ou senha inválida')
+    } else {
+        alert('redirecionando para pagina')
     }
 }
 )
+
