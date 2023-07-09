d0e2f02ac6bb647785199c0c2ef52d6f7c8db76c
Charlie-Pencal <cpencal@gmail.com>
2023-07-09 19:42:45 -0300

    fix validação de comprimento de senha

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const botao = document.getElementById('botao')
 
 botao.addEventListener('click', function (botao) {
     botao.preventDefault();
-    if (login.value.indexOf('@') === -1 || login.value.indexOf('@') === 0 || login.value.indexOf('@') === login.value.length - 1 || login.value.indexOf('.') === -1 || login.value.indexOf('.') === 0 || login.value.indexOf('.') === login.value.length - 1 || login.value.length < 7) {
+    if (login.value.indexOf('@') === -1 || login.value.indexOf('@') === 0 || login.value.indexOf('@') === login.value.length - 1 || login.value.indexOf('.') === -1 || login.value.indexOf('.') === 0 || login.value.indexOf('.') === login.value.length - 1 || senha.value.length < 7) {
         alert('Login ou senha inválida')
     } else {
         alert('redirecionando para pagina')
