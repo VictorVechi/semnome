a87fe6a08750e729fb0b0e635149e46a4de89f57
VictorVechi <victorhvechi@gmail.com>
2023-07-11 21:06:42 -0300

    alerta de recovery de senha

--- src/views/recuperacao-script.js
+++ src/views/recuperacao-script.js
@@ -8,8 +8,8 @@ enviar.addEventListener('click', function (enviar) {
     if (email.value.indexOf('@') === -1 || email.value.indexOf('@') === 0 || email.value.indexOf('@') === email.value.length - 1 || email.value.indexOf('.') === -1 || email.value.indexOf('.') === 0 || email.value.indexOf('.') === email.value.length - 1) {
         alert('Login ou senha inválida')
     } else {
+        alert("Recuperação de senha enviada com sucesso")
         window.location.href = '../pages/login.html'
-
     }
 }
 )
